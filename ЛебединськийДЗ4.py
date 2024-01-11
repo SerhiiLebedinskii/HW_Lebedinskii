753ecb9a4ab0b1daed13342203e068c96b0afac3
SerhiiLebedinskii <144608984+SerhiiLebedinskii@users.noreply.github.com>
2024-01-12 00:22:32 +0200

    Виконав 1 завдання 4 практичної роботи

--- ЛебединськийДЗ4.py
+++ ЛебединськийДЗ4.py
@@ -1,4 +1,28 @@
 #Задача 1. Курьер
+need_apartment = int(input("Введіть номер квартири до якої потрібно потрапити:"))
+while need_apartment == 0 or need_apartment > 30:
+    need_apartment = int(input("Введіть номер квартири до якої потрібно потрапити:"))
+else:
+    floor_lst = [1,2,3,4,5]
+    apartment_lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+    entrance_lst = [1,2,3]
+    number_apartment = need_apartment
+    def information_apartment (number_apartment):
+        if 1 <= number_apartment <= 10:
+            entrance = entrance_lst[0]
+            floor = (number_apartment-1)//2+1
+        elif 11 <= number_apartment <= 20:
+            entrance = entrance_lst[1]
+            floor = (number_apartment-11)//2+1
+        else:
+            entrance = entrance_lst[2]
+            floor = (number_apartment-21)//2+1
+        return [floor, entrance]
+information = information_apartment(number_apartment)
+print(f"Номер квартири {number_apartment}, поверх на якому знаходиться квартира {information[0]}, під'їзд квартири {information[1]}")
+
 #Задача 2. Бриллиант
 num = int(input("num then enter: "))
 if num % 2 and num > 0:
@@ -10,4 +34,5 @@ if num % 2 and num > 0:
             spaces = " " * ((num - i) // 2)
             star = "*" * i
             print(spaces, star)
+
 #Задача 3. Файл-тест. Есть файл, в котором хранятся числа в следующем формате:
